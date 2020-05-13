4bc918438791a6b208ae3d8bf89b9b347eac9a85
Bianca Canlas <bcanlas87@gmail.com>
2020-05-13 03:55:59 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,9 +2,18 @@
 def countdown(number)
   while number > 0
   puts "#{number} SECOND(S)!"
-  sleep (1)
   number -= 1 
  end
   puts "HAPPY NEW YEAR!"
 end
+ 
+ def countdown_with_sleep(number)
+  while number > 0
+  puts "#{number} SECOND(S)!"
+  sleep(1)
+  number -= 1 
+ end
+  puts "HAPPY NEW YEAR!"
+end
+ 
  
